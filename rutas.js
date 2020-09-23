c8f34c297d32b234b9f59844f54e479fe78ffca2
Isaac ramirez velez <ramirezisaac.1518@gmail.com>
2020-09-22 20:49:32 -0600

    ruta para eliminar

--- rutas.js
+++ rutas.js
@@ -11,9 +11,24 @@ router.get("/", (req, res)=>{
 router.post("/nuevo", (req, res)=>{
     coneccionMySql.query('INSERT INTO `hola`(`ID`, `titulo`) VALUES (0,"'+req.body.titulo+'")',  (error) => {
         if (error){console.log(error)}
-        res.json({"mensaje":"ok"})
+        else{
+            res.json({"mensaje":"ok"})
+        }
+        
       })
     
 })
+
+router.delete("/:id/eliminar", (req, res)=>{
+    coneccionMySql.query("DELETE FROM `hola` WHERE `hola`.`ID` = "+req.params.id,  (error) => {
+        if (error){console.log(error)}
+        else{
+            res.json({"mensaje":"eliminado"})
+        }
+      })
+    
+})
+
+
 
 module.exports = router;
